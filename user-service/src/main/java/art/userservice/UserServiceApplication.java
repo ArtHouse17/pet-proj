022c6822c401fb4ff918ac3110f53041f49dfee9
ArtHouse17 <artem.demidov.13@mail.ru>
2024-11-10 23:32:54 +0300

    Added mail-service and user-service

---
+++ user-service/src/main/java/art/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package art.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
